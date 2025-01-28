71916a3f839c0dd1e00daac3e74a91b591b48a3e
Amartya Varma <amartya.varma@gmail.com>
2025-01-28 15:40:57 -0800

    f

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+from orpheus.mm_model_from_colab.model import (
+    OrpheusConfig,
+    OrpheusForConditionalGeneration,
+)
+from orpheus.mm_model_from_colab.utils import OrpheusUtility
+orpheus = OrpheusUtility()
