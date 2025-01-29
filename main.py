d890c46714a61a539538d2f5d71212120bb9dbab
Amartya Varma <amartya.varma@gmail.com>
2025-01-28 16:11:25 -0800

    f

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from transformers import AutoModel, AutoTokenizer, AutoConfig
 AutoConfig.register("orpheus", OrpheusConfig)
 AutoModel.register(OrpheusConfig, OrpheusForConditionalGeneration)
 
-my_orpheus.fast_download_from_hub() 
+orpheus.fast_download_from_hub() 
 model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
 model = AutoModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
