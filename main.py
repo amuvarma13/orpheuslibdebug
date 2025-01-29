7932857e48ec9001e2bc5069f36272b1f7c7e1a7
Amartya Varma <amartya.varma@gmail.com>
2025-01-28 16:10:05 -0800

    f

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from orpheus.mm_model import (
     OrpheusForConditionalGeneration,
     OrpheusUtility
 )
-my_orpheus = OrpheusUtility()
+import librosa
+orpheus = OrpheusUtility()
 
 from transformers import AutoModel, AutoTokenizer, AutoConfig
 AutoConfig.register("orpheus", OrpheusConfig)
@@ -13,3 +14,18 @@ my_orpheus.fast_download_from_hub()
 model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
 model = AutoModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+
+
+# EITHER get inputs from text
+prompt = "What is an example of a healthy breakfast?"
+inputs = orpheus.get_inputs_from_text(prompt)
+print(inputs)
+
+#generate response
+output_tokens = model.generate(
+    **inputs, 
+    max_new_tokens=100, 
+    repetition_penalty=1.1, 
+    temperature=0.7
+    )
