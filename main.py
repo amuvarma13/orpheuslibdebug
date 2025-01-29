7a3fe10ba105f4f24af5f26014fa1dd032f48451
Amartya Varma <amartya.varma@gmail.com>
2025-01-28 16:14:12 -0800

    f

--- main.py
+++ main.py
@@ -15,17 +15,7 @@ model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
 model = AutoModel.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-
-
 # EITHER get inputs from text
-prompt = "What is an example of a healthy breakfast?"
+prompt = "Okay, so what would be an example of a healthier breakfast option then. Can you tell me?"
 inputs = orpheus.get_inputs_from_text(prompt)
 print(inputs)
-
-#generate response
-output_tokens = model.generate(
-    **inputs, 
-    max_new_tokens=100, 
-    repetition_penalty=1.1, 
-    temperature=0.7
-    )
