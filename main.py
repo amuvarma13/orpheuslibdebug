4e95c818c9fed1b54cd9ec79832d6f0bbd874e69
Amartya Varma <amartya.varma@gmail.com>
2025-01-29 18:57:36 -0800

    f

--- main.py
+++ main.py
@@ -18,9 +18,7 @@ model = AutoModel.from_pretrained(model_name).to("cuda").to(torch.bfloat16)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 orpheus.register_auto_model(model)
-print(SPEECH_WAV_PATH)
 waveform, sample_rate = torchaudio.load(SPEECH_WAV_PATH)
-#convert y 
 
 embeds = orpheus.get_inputs(speech=waveform)
 print(embeds)
@@ -29,11 +27,11 @@ print(embeds)
 # prompt = "Okay, so what would be an example of a healthier breakfast option then. Can you tell me?"
 # inputs = orpheus.get_inputs(text=prompt)
 
-# output_tokens = model.generate(
-#     **inputs, 
-#     max_new_tokens=200, 
-#     repetition_penalty=1.1, 
-#     temperature=0.7
-#     )
+output_tokens = model.generate(
+    **inputs, 
+    max_new_tokens=200, 
+    repetition_penalty=1.1, 
+    temperature=0.7
+    )
 
-# print(tokenizer.decode(output_tokens[0], skip_special_tokens=True))
+print(tokenizer.decode(output_tokens[0], skip_special_tokens=True))
