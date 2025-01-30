8151397277d3681af17acd4136ff18fb9cb2c148
Amartya Varma <amartya.varma@gmail.com>
2025-01-29 21:42:33 -0800

    f

--- main.py
+++ main.py
@@ -18,23 +18,19 @@ model = AutoModel.from_pretrained(model_name).to("cuda").to(torch.bfloat16)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 orpheus.register_auto_model(model)
+
+conversation = orpheus.initialise_conversation_model() # initialise a new conversation
+from orpheus.mm_model.assets import SPEECH_WAV_PATH
+
+import torchaudio
 waveform, sample_rate = torchaudio.load(SPEECH_WAV_PATH)
 
-inputs = orpheus.get_inputs(speech=waveform)
+first_message = {
+    "format":"speech",
+    "data": y
+}
 
-# EITHER get inputs from text
-# prompt = "Okay, so what would be an example of a healthier breakfast option then. Can you tell me?"
-# inputs = orpheus.get_inputs(text=prompt)
+conversation.append_message(first_message)
 
-output_tokens = model.generate(
-    **inputs, 
-    max_new_tokens=200, 
-    repetition_penalty=1.1, 
-    temperature=0.7
-    )
-
-print(tokenizer.decode(output_tokens[0], skip_special_tokens=True))
-
-results = orpheus.parse_output_tokens(output_tokens)
+results = conversation.generate_response()
 print(results)
-
