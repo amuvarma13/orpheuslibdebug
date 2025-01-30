e3b7288252761f176f6d89f0495f09d28d6972f1
Amartya Varma <amartya.varma@gmail.com>
2025-01-29 18:53:53 -0800

    f

--- main.py
+++ main.py
@@ -10,16 +10,14 @@ import torch
 import torchaudio
 orpheus = OrpheusUtility()
 
-# from transformers import AutoModel, AutoTokenizer, AutoConfig
-# AutoConfig.register("orpheus", OrpheusConfig)
-# AutoModel.register(OrpheusConfig, OrpheusForConditionalGeneration)
+from transformers import AutoModel, AutoTokenizer, AutoConfig
 
-# orpheus.initialise()
-# model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
-# model = AutoModel.from_pretrained(model_name).to("cuda").to(torch.bfloat16)
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
+orpheus.initialise()
+model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
+model = AutoModel.from_pretrained(model_name).to("cuda").to(torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-
+orpheus.register_auto_model(model)
 print(SPEECH_WAV_PATH)
 waveform, sample_rate = torchaudio.load(SPEECH_WAV_PATH)
 #convert y 
