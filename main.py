c2ef52ca8e24d8862c5ab24cd5bd241e5e937a04
Amartya Varma <amartya.varma@gmail.com>
2025-01-29 17:47:52 -0800

    f

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from orpheus.mm_model import (
     OrpheusConfig,
     OrpheusForConditionalGeneration,
-    OrpheusUtility
+    OrpheusUtility, 
+    assets
 )
 import librosa
 import torch
@@ -16,15 +17,18 @@ model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
 model = AutoModel.from_pretrained(model_name).to("cuda").to(torch.bfloat16)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+
+print(assets)
+
 # EITHER get inputs from text
-prompt = "Okay, so what would be an example of a healthier breakfast option then. Can you tell me?"
-inputs = orpheus.get_inputs(text=prompt)
+# prompt = "Okay, so what would be an example of a healthier breakfast option then. Can you tell me?"
+# inputs = orpheus.get_inputs(text=prompt)
 
-output_tokens = model.generate(
-    **inputs, 
-    max_new_tokens=200, 
-    repetition_penalty=1.1, 
-    temperature=0.7
-    )
+# output_tokens = model.generate(
+#     **inputs, 
+#     max_new_tokens=200, 
+#     repetition_penalty=1.1, 
+#     temperature=0.7
+#     )
 
-print(tokenizer.decode(output_tokens[0], skip_special_tokens=True))
+# print(tokenizer.decode(output_tokens[0], skip_special_tokens=True))
