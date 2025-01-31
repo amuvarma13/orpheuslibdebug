8384c7b1c2b68c6f20d26cfb87cb8dae36027ee2
Amartya Varma <amartya.varma@gmail.com>
2025-01-30 20:41:54 -0800

    f

--- train.py
+++ train.py
@@ -1,13 +1,10 @@
 from orpheus import OrpheusTrainer
 
-orpheus = OrpheusTrainer()
-
 speech_dataset_name = "amuvarma/5k-qa-pairs-tttts"
 text_dataset_name = "amuvarma/va-320k-330k-snac-no-identity-QA_TTTTS"
 
 model_name = "amuvarma/3b-10m-pretrain-full"
-
-orpheus.initialise(
+orpheus = OrpheusTrainer(
     stage = "stage_1",
     speech_dataset_name = speech_dataset_name,
     text_dataset_name = text_dataset_name, # optional, defaults to generic QA dataset for LLM tuning
