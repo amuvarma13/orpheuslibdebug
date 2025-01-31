cacc644ff454b2048d871357bbce3345446745a9
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 15:02:56 -0800

    f

--- train.py
+++ train.py
@@ -6,14 +6,15 @@ wandb.init(project="orpheusdeblib", name="s1")
 dataset_name = "amuvarma/flattened-convos-regzuck"
 
 model_name = "amuvarma/3b-10m-pretrain-full"
+
+#
 orpheus = OrpheusTrainer(
-    stage = "stage_2",
-    dataset_name = dataset_name,
+    stage = "stage_3",
     model_name = model_name # optional, defaults to Canopy's pretrained model
 )
 
 print("finished initialising")
-orpheus_trainer = orpheus.create_trainer(report_to = "wandb",) # pass in any trainer args here
+orpheus_trainer = orpheus.create_trainer(report_to = "wandb") # pass in any trainer args here
 
 print("created trainer")
 orpheus_trainer.train() #inherits from Trainer
