2dcadd78a5a56166eded55ae76054f61388889f3
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 23:16:11 -0800

    f

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 from orpheus.src.orpheus import OrpheusTrainer, OrpheusDataProcessor
+from datasets.features import Audio
 
+# Assuming your dataset has a 'question_audio' column
 
 dataset_name = "amuvarma/flattened-convos-regzuck"
 
@@ -8,6 +10,9 @@ data_processor = OrpheusDataProcessor()
 dataset = data_processor.fast_load_dataset(dataset_name)
 dataset = dataset.select(range(20))
 processed_dataset = data_processor.adapt_stage_1_to_stage_5_dataset(dataset)
+processed_dataset = processed_dataset.cast_column('question_audio', Audio())
+
+
 
 processed_dataset.push_to_hub("test-ds-kok-proc")
 
