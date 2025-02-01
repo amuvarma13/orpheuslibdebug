e2a6004a7cefdd60cdfd500e551aa4f93cbb9675
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 23:30:15 -0800

    f

--- data.py
+++ data.py
@@ -1,27 +1,21 @@
-from orpheus.src.orpheus import OrpheusTrainer, OrpheusDataProcessor
-from datasets.features import Audio
+from orpheus.src.orpheus import OrpheusTrainer
+import wandb
 
-# Assuming your dataset has a 'question_audio' column
+wandb.init(project="orpheusdeblib", name="s4")
 
-dataset_name = "amuvarma/flattened-convos-regzuck"
+dataset_name = "amuvarma/test-ds-kok-proc"
+model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
 
-data_processor = OrpheusDataProcessor()
-
-dataset = data_processor.fast_load_dataset(dataset_name)
-dataset = dataset.select(range(20))
-processed_dataset = data_processor.adapt_stage_1_to_stage_5_dataset(dataset)
-processed_dataset = processed_dataset.cast_column('question_audio', Audio())
-
-
-
-processed_dataset.push_to_hub("test-ds-kok-proc")
-
-orpheus = OrpheusTrainer(    
-    stage = "stage_5",
-    dataset = processed_dataset, 
-    model_name = "amuvarma/zuck-3bregconvo-automodelcompat" # pass a 🤗 model or local checkpoint folder)
+#
+orpheus = OrpheusTrainer(
+    stage = "stage_4",
+    model_name = model_name,
+    dataset_name = dataset_name,
+    batch_size = 21, # use batch_size * number_of_gpus = 64 for quickest training
 )
 
-orpheus_trainer = orpheus.create_trainer()
+print("finished initialising")
+orpheus_trainer = orpheus.create_trainer(report_to = "wandb") # pass in any trainer args here
 
-orpheus_trainer.train() # subclasses 🤗 Trainer
+print("created trainer")
+orpheus_trainer.train() #inherits from Trainer
