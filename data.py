0e7fa3b2be5837154554c70eeb00b32ac6f56b4c
Amartya Varma <amartya.varma@gmail.com>
2025-02-01 10:56:31 -0800

    f

--- data.py
+++ data.py
@@ -4,26 +4,26 @@ from orpheus.src.orpheus import OrpheusTrainer, OrpheusDataProcessor
 
 # wandb.init(project="orpheusdeblib", name="s5")
 
-dataset_name = "amuvarma/flattened-convos-regzuck"
-# model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
+dataset_name = "amuvarma/test-ds-kok-proc-1"
+model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
 
 data_processor = OrpheusDataProcessor()
 dataset = data_processor.fast_load_dataset(dataset_name)
-dataset = dataset.select(range(200))
+# dataset = dataset.select(range(200))
 
-processed_dataset = data_processor.adapt_stage_1_to_stage_5_dataset(dataset)
+# processed_dataset = data_processor.adapt_stage_1_to_stage_5_dataset(dataset)
 
-processed_dataset.push_to_hub("amuvarma/test-ds-kok-proc-1")
+# processed_dataset.push_to_hub("amuvarma/test-ds-kok-proc-1")
 
-# orpheus = OrpheusTrainer(
-#     stage = "stage_5",
-#     model_name = model_name,
-#     dataset = dataset,
-#     batch_size = 21, # use batch_size * number_of_gpus = 64 for quickest training
-# )
+orpheus = OrpheusTrainer(
+    stage = "stage_5",
+    model_name = model_name,
+    dataset = dataset,
+    batch_size = 21, # use batch_size * number_of_gpus = 64 for quickest training
+)
 
 # print("finished initialising")
-# orpheus_trainer = orpheus.create_trainer(report_to = "wandb") # pass in any trainer args here
+orpheus_trainer = orpheus.create_trainer(report_to = "wandb") # pass in any trainer args here
 
 # print("created trainer")
-# orpheus_trainer.train() #inherits from Trainer
+orpheus_trainer.train() #inherits from Trainer
