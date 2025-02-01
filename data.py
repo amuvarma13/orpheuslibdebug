4a592d99acb7f970635571389aa4bfad6baf2f1e
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 21:39:54 -0800

    f

--- data.py
+++ data.py
@@ -1,29 +1,21 @@
 from orpheus.src.orpheus import OrpheusTrainer, OrpheusDataProcessor
 
-dataset_name = "amuvarma/flattened-convos-regzuck"
+
+
+dataset_name = "amuvarma/zuck-3bregconvo-automodelcompat"
 
 data_processor = OrpheusDataProcessor()
 
 dataset = data_processor.fast_load_dataset(dataset_name)
-
+dataset = dataset.select(range(100))
 processed_dataset = data_processor.adapt_stage_1_to_stage_5_dataset(dataset)
 
-print(processed_dataset)
-# orpheus = OrpheusTrainer()
+orpheus = OrpheusTrainer(    
+    stage = "stage_5",
+    dataset = processed_dataset, 
+    model_name = "amuvarma/stage-4-tuned-example-model" # pass a 🤗 model or local checkpoint folder)
+)
 
+orpheus_trainer = orpheus.create_trainer()
 
-# dataset_name = "amuvarma/orpheus_stage_1"
-
-
-
-
-
-# orpheus.initialise(
-#     stage = "stage_5",
-#     dataset = processed_dataset, 
-#     model_name = "amuvarma/stage-4-tuned-example-model" # pass a 🤗 model or local checkpoint folder
-# )
-
-# orpheus_trainer = orpheus.create_trainer() 
-
-# orpheus_trainer.train() # subclasses 🤗 Trainer
+orpheus_trainer.train() # subclasses 🤗 Trainer
