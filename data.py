3fe4ce3581b0055afe4581aa43d32200505adb1c
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 23:36:08 -0800

    f

--- data.py
+++ data.py
@@ -1,16 +1,19 @@
-from orpheus.src.orpheus import OrpheusTrainer
+from orpheus.src.orpheus import OrpheusTrainer, OrpheusDataProcessor
 import wandb
+
 
 wandb.init(project="orpheusdeblib", name="s4")
 
 dataset_name = "amuvarma/test-ds-kok-proc"
 model_name = "amuvarma/zuck-3bregconvo-automodelcompat"
 
-#
+data_processor = OrpheusDataProcessor()
+dataset = data_processor.fast_load_dataset(dataset_name)
+
 orpheus = OrpheusTrainer(
-    stage = "stage_4",
+    stage = "stage_5",
     model_name = model_name,
-    dataset_name = dataset_name,
+    dataset = dataset,
     batch_size = 21, # use batch_size * number_of_gpus = 64 for quickest training
 )
 
