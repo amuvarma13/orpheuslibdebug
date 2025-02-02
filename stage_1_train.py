6365f5acbcc71e82d304bb731dadf7554508ea50
Amartya Varma <amartya.varma@gmail.com>
2025-02-02 14:20:35 -0800

    f

--- stage_1_train.py
+++ stage_1_train.py
@@ -1,25 +1,25 @@
 from orpheus import OrpheusTrainer
-# import wandb 
-# wandb.init(project="orpheusdeblib", name="s1")
+import wandb 
+wandb.init(project="orpheusdeblib", name="s1")
 
 
 
 
-# speech_dataset_name = "amuvarma/luna-6k"
-# text_dataset_name = "amuvarma/10k-qa"
+speech_dataset_name = "amuvarma/luna-6k"
+text_dataset_name = "amuvarma/10k-qa"
 
-# model_name = "amuvarma/3b-10m-pretrain-full"
+model_name = "amuvarma/3b-10m-pretrain-full"
 
 
-# orpheus = OrpheusTrainer(
-#     stage = "stage_1",
-#     speech_dataset_name = speech_dataset_name,
-#     text_dataset_name = text_dataset_name, # optional, defaults to generic QA dataset for LLM tuning
-#     model_name = model_name # optional, defaults to Canopy's pretrained model
-# )
+orpheus = OrpheusTrainer(
+    stage = "stage_1",
+    speech_dataset_name = speech_dataset_name,
+    text_dataset_name = text_dataset_name, # optional, defaults to generic QA dataset for LLM tuning
+    model_name = model_name # optional, defaults to Canopy's pretrained model
+)
 
-# orpheus_trainer = orpheus.create_trainer(
-#   report_to = "wandb" # pass any 🤗 TrainingArgs in here
-# ) 
+orpheus_trainer = orpheus.create_trainer(
+  report_to = "wandb" # pass any 🤗 TrainingArgs in here
+) 
 
-# orpheus_trainer.train() # orpheus_trainer subclasses 🤗 Trainer
+orpheus_trainer.train() # orpheus_trainer subclasses 🤗 Trainer
