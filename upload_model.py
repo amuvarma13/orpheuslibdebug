deb91ffb7a52defeb26c2b3a43901654416be8fe
Amartya Varma <amartya.varma@gmail.com>
2025-02-03 15:10:33 -0800

    f

--- upload_model.py
+++ upload_model.py
@@ -1,6 +1,6 @@
 from orpheus.src.orpheus import OrpheusUtility
 orpheus = OrpheusUtility()
 
-checkpoint_name = "checkpoints/checkpoint-5996" # find <TRAINING STEPS> in checkpoints/
-push_name = "amuvarma/canopy-tune-stage_1-luna"
+checkpoint_name = "checkpoints/checkpoint-700" # find <TRAINING STEPS> in checkpoints/
+push_name = "amuvarma/canopy-tune-stage_2-luna"
 orpheus.fast_push_to_hub(checkpoint=checkpoint_name, push_name=push_name)
