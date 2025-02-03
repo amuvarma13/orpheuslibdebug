5ddc9619363732603a4703271f908e22fa50eace
Amartya Varma <amartya.varma@gmail.com>
2025-02-03 14:34:06 -0800

    f

--- stage_2_train.py
+++ stage_2_train.py
@@ -1,6 +1,6 @@
 from orpheus.src.orpheus import OrpheusTrainer
 import wandb 
-wandb.init(project="orpheus-luna-stage2", name="r1")
+wandb.init(project="orpheus-luna-stage2", name="r2")
 
 dataset_name = "amuvarma/luna-convos"
 model_name = "amuvarma/canopy-tune-stage_1-luna"
