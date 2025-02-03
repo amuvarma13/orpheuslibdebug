0dbf44352ff20d2e61fcf1fca4bc23acdd22bd6a
Amartya Varma <amartya.varma@gmail.com>
2025-02-03 11:24:49 -0800

    f

---
+++ stage_2_train.py
@@ -0,0 +1,18 @@
+from orpheus.src.orpheus import OrpheusTrainer
+import wandb 
+wandb.init(project="orpheus-luna-stage2", name="r0")
+
+orpehus = OrpheusTrainer()
+
+dataset_name = "amuvarma/luna-convos"
+model_name = "amuvarma/canopy-tune-stage_1-luna"
+
+orpheus = OrpheusTrainer(
+    stage = "stage_2",
+    dataset_name = dataset_name,
+    model_name = model_name # optional, defaults to Canopy's pretrained model
+)
+
+orpheus_trainer = orpheus.create_trainer(report_to = "wandb") # pass any 🤗 TrainingArgs in here
+
+orpheus_trainer.train()  # orpheus_trainer subclasses 🤗 Trainer
