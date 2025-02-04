944ffdbf6982c1bffd9ce6f92f2bf5095010b1b5
Amartya Varma <amartya.varma@gmail.com>
2025-02-03 18:11:13 -0800

    f

---
+++ stage_5_train.py
@@ -0,0 +1,19 @@
+from orpheus.src.orpheus import OrpheusTrainer, OrpheusDataProcessor
+
+data_processor = OrpheusDataProcessor()
+
+speech_dataset_name = "amuvarma/luna-6k"
+
+dataset = data_processor.fast_load_dataset(speech_dataset_name)
+
+processed_dataset = data_processor.adapt_stage_1_to_stage_5_dataset(dataset)
+
+orpheus = OrpheusTrainer(    
+    stage = "stage_5",
+    dataset = processed_dataset, 
+    model_name = "amuvarma/canopy-tune-stage_4-luna" # pass a 🤗 model or local checkpoint folder)
+)
+
+orpheus_trainer = orpheus.create_trainer() 
+
+orpheus_trainer.train() # subclasses 🤗 Trainer
