5041de71333328fc7f98ccee9e306e5406cd21f9
Amartya Varma <amartya.varma@gmail.com>
2025-02-03 16:23:07 -0800

    f

---
+++ stage_4_train.py
@@ -0,0 +1,13 @@
+from orpheus.src.orpheus import OrpheusTrainer
+
+model_name = "checkpoints/checkpoint-7813" # from stage_2_train.py
+#** loading the datasets can take a while, even up to 30 mins **
+orpheus = OrpheusTrainer(
+    stage = "stage_4",
+    model_name = model_name,
+    batch_size = 8, # use batch_size * number_of_gpus = 64 for quickest training
+)
+
+orpheus_trainer = orpheus.create_trainer( report_to="wandb" ) # subclasses 🤗 Trainer 
+
+orpheus_trainer.train()
