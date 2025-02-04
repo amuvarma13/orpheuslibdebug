d426c938036266c3a75aad90cf9ed862dfa74f37
Amartya Varma <amartya.varma@gmail.com>
2025-02-03 18:35:14 -0800

    f

--- stage_5_train.py
+++ stage_5_train.py
@@ -5,7 +5,6 @@ data_processor = OrpheusDataProcessor()
 speech_dataset_name = "amuvarma/luna-6k"
 
 dataset = data_processor.fast_load_dataset(speech_dataset_name)
-dataset = dataset.select(range(10))
 
 processed_dataset = data_processor.adapt_stage_1_to_stage_5_dataset(dataset)
 
